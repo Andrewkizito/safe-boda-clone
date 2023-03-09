d8795255cb76e7727801398a82bbf6049e822df1
AndrewKizito <AndrewKizito54@gmail.com>
2023-03-09 14:17:27 +0300

    Added Tab Items

--- src/navigators/Root.tsx
+++ src/navigators/Root.tsx
@@ -5,6 +5,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // Importing screens
 import Home from "../screens/Home/Home";
 
+// Importing core components
+import { Ionicons } from "@expo/vector-icons";
+import * as colors from "../components/colors";
+
 const Tab = createBottomTabNavigator();
 
 export default function Root() {
@@ -12,10 +16,60 @@ export default function Root() {
         <NavigationContainer>
             <Tab.Navigator
                 initialRouteName="Home"
-                screenOptions={{ headerShown: false }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        if (route.name === "Orders") {
+                            return (
+                                <Ionicons
+                                    name={
+                                        focused
+                                            ? "ios-list"
+                                            : "ios-list-outline"
+                                    }
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        } else if (route.name === "Home") {
+                            return (
+                                <Ionicons
+                                    name={focused ? "home" : "home-outline"}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        } else if (route.name === "My Account") {
+                            return (
+                                <Ionicons
+                                    name={
+                                        focused
+                                            ? "person-circle-sharp"
+                                            : "person-circle-outline"
+                                    }
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        }
+                    },
+                    tabBarActiveTintColor: colors.primary,
+                    tabBarInactiveTintColor: colors.plainIcon,
+                    headerShown: false,
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        fontWeight: "600",
+                    },
+                    tabBarItemStyle: {
+                        paddingVertical: 5,
+                    },
+                    tabBarStyle: {
+                        height: 60,
+                    },
+                })}
             >
+                <Tab.Screen name="Orders" component={Home} />
                 <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Settings" component={Home} />
+                <Tab.Screen name="My Account" component={Home} />
             </Tab.Navigator>
         </NavigationContainer>
     );
