f33b1e3958fc0a0e21c8e0fc9b1c3e88daef3a73
AndrewKizito <AndrewKizito54@gmail.com>
2023-03-09 11:01:55 +0300

    Added AWS Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "safeboda7e5e4139": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
